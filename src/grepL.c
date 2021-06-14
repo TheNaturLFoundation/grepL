743b23cdf4034f42fc49954695096af1693227cf
Simon Scatton <simon.scatton@epita.fr>
2021-06-14 10:43:08 +0200

    Optimized the grep to search using threads... missing functionalities...

--- src/grepL.c
+++ src/grepL.c
@@ -1,25 +1,23 @@
+#define _XOPEN_SOURCE 700
+
 #include <dirent.h>
 #include <err.h>
+#include <fcntl.h>
+#include <ftw.h>
+#include <pthread.h>
 #include <rationl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/mman.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include <fcntl.h>
-#include <sys/mman.h>
 
 #define RED "\033[0;31m"
 #define WHITE "\033[0;37m"
 #define USAGE printf("Usage : grepL [OPTIONS]... PATTERN [FILE/FOLDER]\n")
 #define HELP printf("Try 'grepL -h' for more information\n")
-
-typedef struct FileLine
-{
-    unsigned int line_num;
-    char *line_content;
-} FileLine;
 
 typedef struct Options
 {
@@ -31,17 +29,22 @@ typedef struct Options
 
 } Options;
 
-Options *init_options()
-{
-    Options *op = malloc(sizeof(Options));
-    op->recursion = 0;
-    op->occurences = 0;
-    op->printlines = 0;
-    op->entire_text = 0;
-    op->change_text = 0;
+static reg_t regex;
 
-    return op;
-}
+char buffer[1000] = {0};
+
+static Options option = {
+    .recursion = 0,
+    .occurences = 0,
+    .printlines = 0,
+    .entire_text = 0,
+    .change_text = 0,
+};
+
+static pthread_mutex_t STDOUT_MUTEX = PTHREAD_MUTEX_INITIALIZER;
+
+static int finished = 0;
+
 
 void help()
 {
@@ -54,67 +57,6 @@ void help()
     printf("   -e : print the entire text\n");
     printf("   -c : change the regexp with the new string\n");
     printf("        grepL -c PATTERN REPLACEMENT [FILE/FOLDER]\n");
-
-}
-
-FileLine* getfilelines(char *file, int* total_lines)
-{
-    int fd;
-    char *lhs;
-    char *rhs;
-    char *cp;
-    int len;
-    int linecnt;
-    int linemax;
-    FileLine *linelist;
-    FileLine *line;
-    struct stat st;
-    char *fbuf;
-    char cbuf[50000];
-
-    fd = open(file,O_RDONLY);
-    if(fd == -1)
-        return NULL;
-
-    fstat(fd,&st);
-
-    fbuf = mmap(NULL,st.st_size,PROT_READ,MAP_PRIVATE,fd,0);
-
-    linecnt = 0;
-    linemax = 0;
-    linelist = NULL;
-
-    lhs = fbuf;
-    rhs = fbuf;
-
-    for (lhs = fbuf;  lhs < &fbuf[st.st_size];  lhs = rhs + 1) {
-        rhs = strchr(lhs,'\n');
-
-        if (rhs == NULL)
-            break;
-
-        len = rhs - lhs;
-
-        if ((linecnt + 1) > linemax) {
-            linemax += 100;
-            linelist = realloc(linelist,linemax * sizeof(FileLine));
-        }
-
-        line = &linelist[linecnt];
-        line->line_num = linecnt++;
-
-        cp = malloc(len + 1);
-        memcpy(cp,lhs,len);
-        cp[len] = 0;
-        line->line_content = cp;
-    }
-
-    munmap(fbuf,st.st_size);
-    close(fd);
-    *total_lines = linecnt;
-    linelist = realloc(linelist, linecnt * sizeof(FileLine));
-
-    return linelist;
 }
 
 void replace(char *path, reg_t regexp, char *text)
@@ -135,51 +77,7 @@ void replace(char *path, reg_t regexp, char *text)
     printf("done\n");
 }
 
-void search_lines(reg_t regexp, char *path, Options *options)
-{
-
-    int line_count = 0;
-    FileLine* lines = getfilelines(path, &line_count);
-    if(lines == NULL)
-    {
-        printf("No such file or directory: %s\n", path);
-        return;
-    }
-    for (int i = 0; i < line_count; i++)
-    {
-        match **match_list;
-        size_t match_size = 0;
-        size_t position = 0;
-        FileLine *line = lines + i;
-        if ((match_size = regex_search(regexp, line->line_content, &match_list)) != 0)
-        {
-            if (options->recursion)
-                printf("\033[0;35m%s:"WHITE, path);
-            if (options->printlines)
-                printf("\033[0;32m%i:"WHITE, line->line_num);
-            options->occurences += match_size;
-            char *l = line->line_content;
-            for (size_t i = 0; i < match_size; i++)
-            {
-                match *match = match_list[i];
-                printf("%.*s", (int)(match->start - position), l);
-                l = line->line_content + match->start;
-                position = match->start;
-                printf(RED "%.*s" WHITE, (int)(match->length), l);
-                l += match->length;
-                position += match->length;
-                if (i + 1 >= match_size)
-                {
-                    printf("%s\n", l);
-                }
-
-            }
-        }
-        else if (options->entire_text)
-            printf("%s\n", line->line_content);
-    }
-}
-
+/*
 void get_files(char *path, reg_t regexp, Options *options)
 {
     DIR *d = opendir(path);
@@ -189,6 +87,7 @@ void get_files(char *path, reg_t regexp, Options *options)
         return;
     }
     struct dirent *dir;
+    int fd = open(file, O_RDONLY);
     while ((dir = readdir(d)) != NULL)
     {
         if (dir->d_type != DT_DIR)
@@ -211,9 +110,86 @@ void get_files(char *path, reg_t regexp, Options *options)
     closedir(d);
 }
 
+*/
+
+struct file_data
+{
+    struct stat *sb;
+    char *path;
+    reg_t regexp;
+};
+
+void *read_file(void *data)
+{
+    struct file_data *d = data;
+    char *lhs;
+    char *rhs;
+    char *cp;
+    int len;
+    int linecnt;
+    int linemax;
+    FILE *f = fopen(d->path, "r");
+    if (f == NULL)
+        return NULL;
+    char *fbuf = malloc(sizeof(char) * (d->sb->st_size + 1));
+    fread(fbuf, d->sb->st_size, 1, f);
+    fclose(f);
+    match **match_list;
+    size_t match_size = 0;
+    size_t position = 0;
+    if ((match_size = regex_search(d->regexp, fbuf, &match_list)) != 0)
+    {
+        option.occurences += match_size;
+        for (size_t i = 0; i < match_size; i++)
+        {
+            if (option.recursion)
+            {
+                printf("%s:", d->path);
+            }
+            if (option.printlines)
+            {
+                printf("033[0;32m%i:" WHITE, 2);
+            }
+            match *match = match_list[i];
+            printf(RED "%.*s\n" WHITE, (int)(match->length),
+                   fbuf + match->start);
+        }
+    }
+    else if (option.entire_text)
+        printf("%s\n", fbuf);
+    free(fbuf);
+    free(data);
+    return NULL;
+}
+
+int process_file(const char *filepath, const struct stat *info,
+                 const int typeflag, struct FTW *pathinfo)
+{
+    if (typeflag == FTW_F && info->st_size != 0)
+    {
+        struct file_data *arg = malloc(sizeof(struct file_data));
+        char *copy = malloc(1 + strlen(filepath));
+        struct stat *info_cpy = malloc(sizeof(struct stat));
+        memcpy(info_cpy, info, sizeof(struct stat));
+        arg->sb = info_cpy;
+        strcpy(copy, filepath);
+        arg->path = copy;
+        arg->regexp = regex;
+        pthread_t thr;
+        pthread_create(&thr, NULL, read_file, arg);
+        pthread_detach(thr);
+    }
+    return 0;
+}
+
+void recurse_directories(const char *const dirpath)
+{
+    nftw(dirpath, process_file, 200, FTW_PHYS);
+    finished = 1;
+}
+
 int main(int argc, char **argv)
 {
-    Options *options = init_options();
     int opt;
 
     if (argc <= 2 && !(argc == 2 && strcmp(argv[1], "-h") == 0))
@@ -228,30 +204,30 @@ int main(int argc, char **argv)
     {
         switch (opt)
         {
-            case 'h':
-                help();
-                return 0;
-                break;
-            case 'r':
-                options->recursion = 1;
-                break;
-            case 'o':
-                options->occurences = 1;
-                break;
-            case 'l':
-                options->printlines = 1;
-                break;
-            case 'e':
-                options->entire_text = 1;
-                break;
-            case 'c':
-                options->change_text = optind-1;
+        case 'h':
+            help();
+            return 0;
+            break;
+        case 'r':
+            option.recursion = 1;
+            break;
+        case 'o':
+            option.occurences = 1;
+            break;
+        case 'l':
+            option.printlines = 1;
+            break;
+        case 'e':
+            option.entire_text = 1;
+            break;
+        case 'c':
+            option.change_text = optind - 1;
 
-                break;
-            case '?':
-                printf("unknown option %c\n", opt);
-                HELP;
-                break;
+            break;
+        case '?':
+            printf("unknown option %c\n", opt);
+            HELP;
+            break;
         }
     }
 
@@ -261,7 +237,7 @@ int main(int argc, char **argv)
         USAGE;
         HELP;
 
-        if (options->change_text)
+        if (option.change_text)
         {
             printf("After -c, indicate the new string:\n");
             printf("    grepL -c PATTERN REPLACEMENT [FILE/FOLDER]\n");
@@ -269,22 +245,19 @@ int main(int argc, char **argv)
         return 0;
     }
 
-    reg_t regexp = regex_compile(argv[optind]);
-
-    char occ = options->occurences;
-    if (options->change_text)
+    regex = regex_compile(argv[optind]);
+    char occ = option.occurences;
+    if (option.change_text)
     {
-        replace(argv[optind+1], regexp, argv[options->change_text]);
+        replace(argv[optind + 1], regex, argv[option.change_text]);
         return 0;
     }
-
-    get_files(argv[optind + 1], regexp, options);
-
-    regex_free(regexp);
-
-    options->occurences--;
+    if (option.recursion)
+    {
+        recurse_directories(argv[optind + 1]);
+    }
+    option.occurences--;
     if (occ)
-        printf("%li occurences\n", options->occurences);
-
-    return 0;
+        printf("%li occurences\n", option.occurences);
+    pthread_exit(0);
 }
